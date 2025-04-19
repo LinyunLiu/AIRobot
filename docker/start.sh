79a98d80817013fefe69cdbd2594da66b8d03a0b
Jacobus Burger <therealjacoburger@gmail.com>
2025-04-19 16:29:58 -0700

    added docker environment for remote machines connecting to it

---
+++ docker/start.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# start docker container with xorg windows and more
+
+xhost +local:$USERNAME
+export ROS_DOMAIN_ID=0
+
+if [ "$(docker ps -aq -f name=airos)" ]
+then
+  if ["$(docker ps -q -f name=airos)" ]
+  then
+    echo "container is running. attaching..."
+  else
+    echo "starting existing container"
+    docker start airos
+  fi
+  docker exec -it \
+      -e DISPLAY=$DISPLAY \
+      -e ROS_DOMAIN_ID=$ROS_DOMAIN_ID \
+      airos bash
+else
+  docker run -it \
+      --name airos \
+      --env DISPLAY=$DISPLAY \
+      --env ROS_DOMAIN_ID=$ROS_DOMAIN_ID \
+      --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+      --volume="$(pwd):/home/rosuser/ros2_ws" \
+      --device=/dev/dri \
+      --group-add video \
+      --net=host \
+      --privileged \
+      ros2 bash
+fi
+
+
